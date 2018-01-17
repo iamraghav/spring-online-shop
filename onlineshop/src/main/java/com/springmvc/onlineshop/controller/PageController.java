0122572ed8b4a2894da137e204e3f21341aa66d6
iamraghav <raghav3992@gmail.com>
2018-01-17 21:05:13 +0530

    Successfully loaded static resources

--- onlineshop/src/main/java/com/springmvc/onlineshop/controller/PageController.java
+++ onlineshop/src/main/java/com/springmvc/onlineshop/controller/PageController.java
@@ -1,7 +1,9 @@
 package com.springmvc.onlineshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -9,10 +11,28 @@ public class PageController {
 
 	@RequestMapping(value = {"/", "/home", "/index"})
 	public ModelAndView index() {
-		
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("greeting", "Welcome to Spring Web MVC");
-		
+		return mv;
+	}
+	
+//	@RequestMapping(value="/test")
+//	public ModelAndView test(@RequestParam(value="greeting", required=false)String greeting) {
+//		if(greeting == null) {
+//			greeting = "Hello there";
+//		}
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("greeting", greeting);
+//		return mv;
+//	}
+	
+	@RequestMapping(value="/test/{greeting}")
+	public ModelAndView test(@PathVariable(value="greeting", required=false)String greeting) {
+		if(greeting == null) {
+			greeting = "Hello there";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
 		return mv;
 	}
 }
